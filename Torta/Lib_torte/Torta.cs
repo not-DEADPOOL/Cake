b18c9da177f696a469f85a004b43b1ae8b62a819
DEAD POOL <78198419+not-DEADPOOL@users.noreply.github.com>
2023-02-09 13:05:14 +0100

    Add files via upload

---
+++ Torta/Lib_torte/Torta.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Lib_torte
+{
+    public class Torta
+    {
+        public string Nome;
+        public double Peso;
+        public double Prezzo { get { return} }
+    }
+}
---
+++ Torta/Torta/Consumer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Lib_torte;
+
+namespace Torta
+{
+    class Consumer
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
